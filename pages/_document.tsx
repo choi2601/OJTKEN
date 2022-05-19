b4ebcdc2a21b0b1e53099d063888ac6f8477d5cc
Ken <bhchoi@maxst.com>
2022-05-19 18:09:10 +0900

    [OJTKEN-2] PR 피드백 수정 - Duplicate plugin/preset 에러 방지를 위한 unique-key 수정
    - meta 태그 _app 내로 위치 변경

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,9 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>OJT-Ken</title>
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700;900&display=swap"
           rel="stylesheet"
