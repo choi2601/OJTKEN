ffd1ad674fb92f51bcf2b9fbd2b6722d30723bdf
Ken <bhchoi@maxst.com>
2022-05-23 23:04:00 +0900

    [OJTKEN-6] react-query 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
 
@@ -7,7 +9,11 @@ import GlobalFont from '@styles/font';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '@styles/theme';
 
+import { QueryClient, QueryClientProvider, Hydrate, Query } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = React.useRef(new QueryClient());
   return (
     <>
       <Head>
@@ -15,11 +21,16 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>OJT-Ken</title>
       </Head>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <GlobalFont />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <GlobalStyle />
+      <GlobalFont />
+      <QueryClientProvider client={queryClient.current}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </Hydrate>
+        <ReactQueryDevtools initialIsOpen={false} position="bottom-left" />
+      </QueryClientProvider>
     </>
   );
 }
