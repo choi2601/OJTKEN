4cd7e1cfee8a884e8b7e020f879ee7ada5909a5e
Ken <bhchoi@maxst.com>
2022-05-25 15:32:00 +0900

    [OJTKEN-9] css wrapping 방식 수정

--- src/components/common/labelToLink/LabelToLinkStyle.ts
+++ src/components/common/labelToLink/LabelToLinkStyle.ts
@@ -1,18 +1,17 @@
 import styled, { css } from 'styled-components';
 
 const LabelToLinkWrapper = styled.li<{ isActive: boolean }>`
-  padding: 25px;
-  border: none;
-  font-size: 17px;
-  transition: ease background-color 200ms;
-  cursor: pointer;
-
   ${({ theme, isActive }) => {
     const { color } = theme;
 
     return css`
+      padding: 25px;
+      border: none;
+      font-size: 17px;
       color: ${color.white};
       background-color: ${isActive ? color.blue[700] : 'inherit'};
+      transition: ease background-color 200ms;
+      cursor: pointer;
 
       &:hover {
         background-color: ${color.blue[500]};
