886b90a7b7edfeb34e3315da3832bba4ccc6e223
Ken <bhchoi@maxst.com>
2022-05-31 09:52:38 +0900

    document 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,47 +1,21 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
-class MyDocument {
-  static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
-        });
-
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      };
-    } finally {
-      sheet.seal();
-    }
-  }
-
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link rel="preload" href="/font/Roboto-Bold.woff" as="font" crossOrigin="" />
-          <link rel="preload" href="/font/Roboto-Regular.woff" as="font" crossOrigin="" />
-          <link rel="preload" href="/font/Roboto-Light.woff" as="font" crossOrigin="" />
-        </Head>
-        <body id="body">
-          <Main />
-          <NextScript />
-          <div id="portal-section" />
-        </body>
-      </Html>
-    );
-  }
+function MyDocument() {
+  return (
+    <Html>
+      <Head>
+        <link rel="preload" href="/font/Roboto-Bold.woff" as="font" crossOrigin="" />
+        <link rel="preload" href="/font/Roboto-Regular.woff" as="font" crossOrigin="" />
+        <link rel="preload" href="/font/Roboto-Light.woff" as="font" crossOrigin="" />
+      </Head>
+      <body id="body">
+        <Main />
+        <NextScript />
+        <div id="portal-section" />
+      </body>
+    </Html>
+  );
 }
 
 export default MyDocument;
